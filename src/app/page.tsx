a50aa9a7ed367261b441aebc037a7867b11eb5e3
firasayari10 <ayari.firas@esprit.tn>
2025-10-02 23:41:42 +0100

    session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,31 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useLogOut } from "@/features/auth/api/use-logout";
+import { useCurrent } from "@/features/auth/api/usecurrent";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 
 export default function Home() {
+    const router = useRouter();
+    const { data , isLoading} = useCurrent();
+    const { mutate } = useLogOut();
+
+    useEffect(()=> {
+        if(!data && !isLoading)
+        {
+            router.push("/sign-in");
+        }
+    },[data]);
+
   return (
     <div>
-      <Button variant="primary">
-          button
-      </Button>
-      <br />
-      <Button variant="destructive">
-          button
-      </Button><br />
-      <Button variant="ghost">
-          button
-      </Button><br />
-      <Button variant="muted">
-          button
-      </Button><br />
-      <Button variant="outline">
-          button
-      </Button><br />
-      <Button variant="secondary">
-          button
-      </Button><br />
-      <Button variant="tertiary">
-          button
-      </Button>
-
-      <Input />
-      
+        Only visible to authrozied Users
+        <Button onClick={()=> mutate()}>
+            LogOut
+        </Button>
+  
     </div>
   );
 }
