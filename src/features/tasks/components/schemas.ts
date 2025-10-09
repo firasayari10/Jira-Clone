f8c40fd2dab951107691721ed49aecc816d98c7c
firasayari10 <ayari.firas@esprit.tn>
2025-10-09 21:00:01 +0100

    Task api

---
+++ src/features/tasks/components/schemas.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+import { TaskStatus } from "./types";
+
+
+export const createTaskSchema= z.object({
+    name:z.string().min(1,"Required"),
+    status: z.nativeEnum(TaskStatus).refine(val => !!val, {
+                message: "Required",
+}),
+    workspaceId: z.string().trim().min(1,"Required"),
+    projectId:z.string().trim().min(1,"Required"),
+    dueDate:z.coerce.date(),
+    assigneeId:z.string().trim().min(1,"Required"),
+    description:z.string().optional(),
+
+})
