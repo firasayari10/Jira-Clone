a49e06840da6ba648a81afd8201e86d54de847c6
firasayari10 <ayari.firas@esprit.tn>
2025-10-16 00:00:06 +0100

    landing page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -0,0 +1,129 @@
+"use client"
+import {CalendarIcon , PlusIcon , SettingsIcon} from"lucide-react";
+import {formatDistanceToNow}from "date-fns";
+import Link from "next/link";
+import { Analytics } from "@/components/analytics";
+import { PageError } from "@/components/page-error";
+import { PageLoader } from "@/components/page-loader";
+import { Button } from "@/components/ui/button";
+import {Card , CardContent} from "@/components/ui/card"
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetProjects } from "@/features/projects/api/use-getprojects";
+import { useCreateProjectModal } from "@/features/projects/hooks/use-create-project-modal";
+import { useGetTasks } from "@/features/tasks/api/use-get-tasks";
+import { useCreateTaskModal } from "@/features/tasks/hooks/use-create-task-modal";
+import { Task } from "@/features/tasks/types";
+import { useGetWorkspaceAnalytics } from "@/features/workspaces/api/use-get-workspace-analytics";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id"
+import { DottedSeperator } from "@/components/dotted-seperator";
+
+
+export const WorkspaceIdClient =()=>{
+    const workspaceId = useWorkspaceId();
+
+    const {data:analytics, isLoading:isLoadingAnalytics}= useGetWorkspaceAnalytics({workspaceId});
+    const {data:tasks, isLoading:isloadingTasks}= useGetTasks({workspaceId});
+    const {data:projects, isLoading:isloadingProjects}= useGetProjects({workspaceId});
+    const {data:members, isLoading:isloadingMembers}= useGetMembers({workspaceId});
+
+
+    const {open:createProject} = useCreateProjectModal();
+    
+
+    const isLoading =  isLoadingAnalytics || isloadingMembers || isloadingProjects || isloadingTasks ;
+
+    if(isLoading){
+        return <PageLoader />
+    }
+
+    if(!analytics || !tasks || !projects || !members)
+    {
+        return <PageError message="Failed to load workspace Data" />
+    }
+  return (
+    <div className="h-full flex flex-col space-y-4">
+        <Analytics  data={analytics}/>
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
+            <TaskList data={tasks.documents} total={tasks.total} />
+
+        </div>
+
+    </div>
+  )
+}
+
+
+interface TaskListProps {
+    data: Task[];
+    total:number;
+}
+
+export const TaskList = ({data, total}:TaskListProps)=>{
+    const workspaceId = useWorkspaceId();
+    const {open: createTask}=useCreateTaskModal();
+    return (
+        <div  className="flex flex-col gap-y-4 col-span-1">
+           <div className="bg-muted rounded-lg p-4">
+            <div className="flex items-center justify-between">
+                <p className="text-lg font-semibold">
+                    Tasks ( {total})
+                </p>
+                <Button variant="muted" size="icon" onClick={createTask}>
+                    <PlusIcon  className="size-4 text-neutral-400"/>
+
+                </Button>
+
+            </div>
+            <DottedSeperator className="my-4"/>
+                <ul className="flex flex-col gap-y-4">
+                    {data.map((task)=>(
+                        <li key={task.$id}>
+                            <Link href={`/workspaces/${workspaceId}/tasks/${task.$id}`}>
+                            <Card className="shadow-none rounded-lg hover:opacity-75 transition">
+                                <CardContent className="p-4">
+                                    <p className="text-lg font-medium truncate">
+                                        {task.name}
+
+                                    </p>
+                                    <div className="flex items-center gap-x-2">
+                                        <p >
+                                            {task.project?.name}
+                                        </p>
+                                        <div className="size-1 rounded-full bg-neutral-300"/>
+                                        <div className="text-sm text-muted-foreground flex items-center">
+                                            <CalendarIcon className="size-4 mr-1"/>
+                                            <span className="truncate">
+                                                {formatDistanceToNow(new Date(task.dueDate))}
+
+                                            </span>
+
+                                        </div>
+
+                                    </div>
+                                </CardContent>
+
+                            </Card>
+                            </Link>
+                        </li>
+                    ))}
+                    <li className="text-sm text-muted-foreground text-center hidden first-of-type:block">
+                        No tasks found
+
+                    </li>
+
+                </ul>
+                <Button variant="muted" className="mt-4 w-full">
+                    <Link href={}>
+                    Show All   
+                    </Link>
+                    
+
+                </Button>
+
+
+           </div>
+
+        </div>
+    )
+
+}
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,15 +2,18 @@ import { zValidator } from "@hono/zod-validator";
 import {Hono} from "hono" ;
 import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABSE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { DATABSE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, PROJECTS_ID, TASKS_ID, WORKSPACES_ID } from "@/config";
 import { ID , Query} from "node-appwrite";
-import { buffer } from "stream/consumers";
+
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
 import { getMember } from "@/features/members/utils";
 import z from "zod";
 import { Workspace } from "../types";
-import { error } from "console";
+
+import { endOfMonth , startOfMonth , subMonths} from "date-fns";
+import { Project } from "@/features/projects/types";
+import { TaskStatus } from "@/features/tasks/types";
 
 const app = new Hono()
 .get("/", sessionMiddleware,
@@ -294,6 +297,195 @@ const app = new Hono()
         return c.json({data: {$id:workspace.$id , name: workspace.name , imageUrl:workspace.imageUrl} })
 
     }
+)
+.get(
+    "/:workspaceId/analytics" ,
+    sessionMiddleware,
+    async(c)=>{
+        const databases=c.get("databases");
+        const user=c.get("user");
+        const {workspaceId} = c.req.param();
+
+        
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id
+        })
+
+        if(!member)
+        {
+            return c.json({error:"Unauthorized"},401)
+        }
+
+        const now = new Date();
+        const thisMonthStart = startOfMonth(now);
+        const thisMonthEnd = endOfMonth(now);
+        const lastMonthStart = startOfMonth(subMonths(now,1));
+        const lastMonthEnd = endOfMonth(subMonths(now,1));
+
+
+        const thisMonthTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.greaterThanEqual("$createdAt",thisMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",thisMonthEnd.toISOString())
+            ]
+        );
+
+        const lastMonthTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.greaterThanEqual("$createdAt",lastMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",lastMonthEnd.toISOString())
+            ]
+        );
+
+        const taskCount = thisMonthTasks.total;
+
+        const taskDifference = taskCount - lastMonthTasks.total;
+
+
+        const thisMonthAssignedTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.equal("assigneeId",member.$id),
+                Query.greaterThanEqual("$createdAt",thisMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",thisMonthEnd.toISOString())
+            ]
+        );
+
+        const lastMonthAssignedTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.equal("assigneeId",member.$id),
+                Query.greaterThanEqual("$createdAt",lastMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",lastMonthEnd.toISOString())
+            ]
+        );
+
+        const assignedTaskCount = thisMonthAssignedTasks.total;
+        const assignedTaskDifference = assignedTaskCount - lastMonthAssignedTasks.total;
+
+
+         const thisMonthIncompleteTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.notEqual("status",TaskStatus.DONE),
+                
+                Query.greaterThanEqual("$createdAt",thisMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",thisMonthEnd.toISOString())
+            ]
+        );
+
+        const lastMonthIncompleteTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.notEqual("status",TaskStatus.DONE),
+                Query.greaterThanEqual("$createdAt",lastMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",lastMonthEnd.toISOString())
+            ]
+        );
+
+        const incompleteTaskCount = thisMonthIncompleteTasks.total;
+
+        const incompleteTaskDifference = incompleteTaskCount - lastMonthIncompleteTasks.total ;
+
+        const thisMonthCompletedTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.equal("status",TaskStatus.DONE),
+                
+                Query.greaterThanEqual("$createdAt",thisMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",thisMonthEnd.toISOString())
+            ]
+        );
+
+        const lastMonthCompletedTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.equal("status",TaskStatus.DONE),
+                Query.greaterThanEqual("$createdAt",lastMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",lastMonthEnd.toISOString())
+            ]
+        );
+
+        const CompleteTaskCount = thisMonthCompletedTasks.total;
+
+        const CompleteTaskDifference = CompleteTaskCount - lastMonthCompletedTasks.total ;
+
+
+
+          const thisMonthOverDueTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.notEqual("status",TaskStatus.DONE),
+                Query.lessThan("dueDate",now.toISOString()),
+                
+                Query.greaterThanEqual("$createdAt",thisMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",thisMonthEnd.toISOString())
+            ]
+        );
+
+        const lastMonthOverDueTasks = await databases.listDocuments(
+            DATABSE_ID,
+            TASKS_ID,
+            [
+                Query.equal("workspaceId",workspaceId),
+                Query.notEqual("status",TaskStatus.DONE),
+                Query.lessThan("dueDate",now.toISOString()),
+                Query.greaterThanEqual("$createdAt",lastMonthStart.toISOString()),
+                Query.lessThanEqual("$createdAt",lastMonthEnd.toISOString())
+            ]
+        );
+
+        const OverDueTaskCount = thisMonthOverDueTasks.total;
+
+        const OverDueTaskDifference = OverDueTaskCount - lastMonthOverDueTasks.total ;
+
+
+
+        return c.json({
+            data:{
+                taskCount,
+                taskDifference,
+                assignedTaskCount,
+                assignedTaskDifference,
+                CompleteTaskCount,
+                CompleteTaskDifference,
+                incompleteTaskCount,
+                incompleteTaskDifference,
+                OverDueTaskCount,
+                OverDueTaskDifference,
+                
+            }
+        })
+
+
+
+
+
+    }
+
+
 )
 
 export default app ; 
